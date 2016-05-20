4a6f7bcd6b2dfbc0455a68414f961305886dd95f
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-20 12:43:29 +0800

    Java8 Install Done

--- Installers/Nukkit-Installer.sh
+++ Installers/Nukkit-Installer.sh
@@ -1,1 +1,6 @@
 #!/bin/sh
+cd ~
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+"http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-linux-x64.rpm"
+sudo yum localinstall jre-8u60-linux-x64.rpm
+rm ~/jre-8u60-linux-x64.rpm #Successfully installed Java8
