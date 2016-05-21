ad74b11b711015bc88e950c7a4eb2ba5b7738d5d
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-21 15:46:17 +0800

    Update Genisys-Installer.sh

--- Installers/Phar-Change/Genisys-Installer.sh
+++ Installers/Phar-Change/Genisys-Installer.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+stop
+rm PocketMine-MP.phar
+wget https://jenkins.mcper.cn/job/Genisys-master/393/artifact/artifact/Genisys_1.1dev-393.phar
+rm 'Genisys_1.1dev-393.phar' PocketMine-MP.phar #After this, Follow the tutorial.
