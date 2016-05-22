faef84839a573ee95f53b4c5af954a090fc6d5fe
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-22 13:15:23 +0800

    Bug dix

--- Installers/Phar-Change/Genisys-Installer.sh
+++ Installers/Phar-Change/Genisys-Installer.sh
@@ -2,4 +2,4 @@
 echo "Running Genisys phar change! please make sure that your server is stopped. and you're in the MCPE Server directory, or it will not work."
 screen -r
 stop
-rm PocketMine-MP.phar && echo "wget https://jenkins.mcper.cn/job/Genisys-master/lastSuccessfulBuild/artifact/artifact/Genisys_1.1dev-396.phar && mv 'Genisys-1.1dev-396.phar' PocketMine-MP.phar" && echo "Genisys Phar Change succeeded! Please starrt your server to see the changes. :D"
+rm PocketMine-MP.phar && wget https://jenkins.mcper.cn/job/Genisys-master/lastSuccessfulBuild/artifact/artifact/Genisys_1.1dev-396.phar && mv 'Genisys-1.1dev-396.phar' PocketMine-MP.phar" && echo "Genisys Phar Change succeeded! Please starrt your server to see the changes. :D"
