7b7ef7d38c85d6597638e540de354c20e8da6bf4
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-22 13:33:05 +0800

    Update MCPEInstall.sh

--- Installers/WIP/MCPEInstall.sh
+++ Installers/WIP/MCPEInstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Running All-In-One MCPE Server Install Script!"
-echo "Hi $USER! What MCPE Server Software would you like to install?"
+echo "Hi $USER! What MCPE Server Software would you like to install?" #Software
 echo "1) PocketMine"
 echo "2) ImagicalMine"
 echo "3) Genisys"
 echo "4) Nukkit"
 echo "5) Nevermind."
-echo "Note: Please enter it using numbers."
+echo "Note: Please enter it using numbers." #1-5
 echo "If you have any Problems/Issues/Questions just go to github.com/CaptainDuck/MCPE-Installers ! :D
 read software;
 
@@ -21,5 +21,5 @@ case $software in
 && wget http://ws.captainduck.xyz/uploads/5/4/8/6/54868769/nukkit-installer.sh;;
 && chmod +x nukkit-installer.sh;;
 && ./nukkit-installer.sh;;
-    5) exit
+    5) exit #To exit
 esac
