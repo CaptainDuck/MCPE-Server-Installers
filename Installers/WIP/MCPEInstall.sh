ad2ae45602f7a6a7089c8d14b2c189e395320b56
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-22 11:40:59 +0800

    75% Done! PocketMine and Nukkit :D

---
+++ Installers/WIP/MCPEInstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Running All-In-One MCPE Server Install Script!"
+echo "Hi $USER! What MCPE Server Software would you like to install?"
+echo "1) PocketMine"
+echo "2) ImagicalMine"
+echo "3) Genisys"
+echo "4) Nukkit"
+echo "5) Nevermind."
+echo "Note: Please enter it using numbers."
+echo "If you have any Problems/Issues/Questions just go to github.com/CaptainDuck/MCPE-Installers ! :D
+read software;
+
+case $software in
+    1) echo "You have selected the PocketMine Software! Installing it right now!" && wget http://ws.captainduck.xyz/uploads/5/4/8/6/54868769/pocketmine-installer.sh && chmod +x pocketmine-installer.sh && ./pocketmine-installer.sh && echo "PocketMine Install Done! :D Good Luck to your new server! :D;;
+    2) echo "You have selected the ImagicalMine Software! Installing it right now.";;
+    3) echo "You have selected the Genisys Software! Installing it right now.";;
+    4) echo "You have selected the Nukkit Software! Installing it right now." && wget http://ws.captainduck.xyz/uploads/5/4/8/6/54868769/nukkit-installer.sh && chmod +x nukkit-installer.sh && ./nukkit-install.sh;;
+    5) exit
+esac
