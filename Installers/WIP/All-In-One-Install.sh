94ceb61eb120c0ccc1c026ca98e6ee88dfabd55d
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-22 11:04:55 +0800

    Update All-In-One-Install.sh

--- Installers/WIP/All-In-One-Install.sh
+++ Installers/WIP/All-In-One-Install.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+echo "Running All-In-One MCPE Server Install Script!"
+echo "Hi $USER! What MCPE Server Software would you like to install?"
+echo "1) PocketMine"
+echo "2) ImagicalMine"
+echo "3) Genisys"
+echo "4) Nukkit"
+echo "5) Nevermind."
+echo "Note: Please enter it using numbers."
+read software;
+
+case $software in
+    1) echo "You have selected the PocketMine Software! Installing it right now.";;
+    2) echo "You have selected the ImagicalMine Software! Installing it right now.";;
+    3) echo "You selected phyton";;
+    4) echo "You selected c++";;
+    5) exit
