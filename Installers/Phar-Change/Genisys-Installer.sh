276ca17c80c5c7c09cb1c34d27095822b702edb7
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-22 16:11:43 +0800

    Genisys Installer ready!

--- Installers/Phar-Change/Genisys-Installer.sh
+++ Installers/Phar-Change/Genisys-Installer.sh
@@ -3,4 +3,4 @@ echo "Running Genisys phar change! please make sure that your server is stopped.
 screen -r
 stop
 rm PocketMine-MP.phar
-wget https://jenkins.mcper.cn/job/Genisys-master/397/artifact/artifact/Genisys_1.1dev-397.phar && mv 'Genisys-1.1dev-397.phar' PocketMine-MP.phar && echo "Genisys Phar Change succeeded! Please starrt your server to see the changes. :D"
+wget https://jenkins.mcper.cn/job/Genisys-master/397/artifact/artifact/Genisys_1.1dev-397.phar && mv 'Genisys_1.1dev-397.phar' PocketMine-MP.phar && echo "Genisys Phar Change succeeded! Please starrt your server to see the changes. :D"
