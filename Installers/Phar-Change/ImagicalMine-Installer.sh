73d4299e34a0208d525126e8e49481905e439685
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-22 13:16:14 +0800

    Bug fix

--- Installers/Phar-Change/ImagicalMine-Installer.sh
+++ Installers/Phar-Change/ImagicalMine-Installer.sh
@@ -2,4 +2,4 @@
 echo "Running ImagicalMine phar change! please make sure that your server is stopped. and you're in the PocketMine Server directory, or it will not work."
 screen -r
 stop
-rm PocketMine-MP.phar && echo "wget http://imagicalcorp.github.io/ImagicalMine/service-download/ImagicalMine.phar && mv 'ImagicalMine.phar' PocketMine-MP.phar" && echo "ImagicalMine Phar Change succeeded! Please starrt your server to see the changes. :D"
+rm PocketMine-MP.phar && wget http://imagicalcorp.github.io/ImagicalMine/service-download/ImagicalMine.phar && mv 'ImagicalMine.phar' PocketMine-MP.phar" && echo "ImagicalMine Phar Change succeeded! Please starrt your server to see the changes. :D"
