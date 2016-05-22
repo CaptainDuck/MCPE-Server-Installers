356b5c226c35d167894d8e43d964f787997cf10f
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-22 12:36:38 +0800

    In testing :3

--- Installers/Phar-Change/Genisys-Installer.sh
+++ Installers/Phar-Change/Genisys-Installer.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo "Installer: Running Genisys Phar Change!"
+echo "Running Genisys phar change! please make sure that your server is stopped. and you're in the MCPE Server directory, or it will not work."
+screen -r
 stop
-rm PocketMine-MP.phar
-wget https://jenkins.mcper.cn/job/Genisys-master/393/artifact/artifact/Genisys_1.1dev-393.phar
-rm 'Genisys_1.1dev-393.phar' PocketMine-MP.phar #After this, Follow the tutorial.
-echo "Installer: Automated install finished, please go to your MCPE server console and/or screen and reload your server."
+rm PocketMine-MP.phar && echo "wget https://jenkins.mcper.cn/job/Genisys-master/lastSuccessfulBuild/artifact/artifact/Genisys_1.1dev-396.phar && mv 'Genisys-1.1dev-396.phar' PocketMine-MP.phar" && echo "Genisys Phar Change succeeded! Please starrt your server to see the changes. :D"
