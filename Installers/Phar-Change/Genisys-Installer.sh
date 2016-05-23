ce5531d40b9d8e2d5105b5923b350adfc175483d
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-23 12:12:37 +0800

    Delete Genisys-Installer.sh

--- Installers/Phar-Change/Genisys-Installer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Running Genisys phar change! please make sure that your server is stopped. and you're in the MCPE Server directory, or it will not work."
-screen -r
-stop
-rm PocketMine-MP.phar
-wget https://jenkins.mcper.cn/job/Genisys-master/397/artifact/artifact/Genisys_1.1dev-397.phar && mv 'Genisys_1.1dev-397.phar' PocketMine-MP.phar && echo "Genisys Phar Change succeeded! Please starrt your server to see the changes. :D"
