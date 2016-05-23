69327f5b79ae84ed30e4a563e64467409ab12422
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-23 12:27:50 +0800

    Update

--- Installers/Phar-Change/ImagicalMine-Installer.sh
+++ Installers/Phar-Change/ImagicalMine-Installer.sh
@@ -9,7 +9,7 @@ read mcpe;
 
 case $mcpe in
     1) echo "Okay! The ImagicalMine Installer will start now :D." && rm PocketMine-MP.phar
-wget https://jenkins.mcper.cn/job/Genisys-master/397/artifact/artifact/Genisys_1.1dev-397.phar && mv 'Genisys-1.1dev-397.phar' PocketMine-MP.phar && echo "ImagicalMine Phar Change succeeded! Please start your server to see the changes. :D";;
+wget http://imagicalcorp.github.io/ImagicalMine/service-download/ImagicalMine.phar && mv 'ImagicalMine.phar' PocketMine-MP.phar && echo "ImagicalMine Phar Change succeeded! Please start your server to see the changes. :D";;
 
     2) echo "Okay! Please make sure of that. If you have completed it, please run the installer again :D.";;
 esac
