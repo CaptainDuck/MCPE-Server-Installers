227d89e1820fe0279a42814995a00ce0e925345c
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-24 18:28:28 +0800

    Update Genisys-Installer.sh

--- Installers/Phar-Change/Genisys-Installer.sh
+++ Installers/Phar-Change/Genisys-Installer.sh
@@ -9,7 +9,7 @@ read mcpe;
 
 case $mcpe in
     1) echo "Okay! The Genisys Installer will start now :D." && rm PocketMine-MP.phar
-wget https://jenkins.mcper.cn/job/Genisys-master/403/artifact/artifact/Genisys_1.1dev-403.phar && mv 'Genisys_1.1dev-403.phar' PocketMine-MP.phar && echo "Genisys Phar Change succeeded! Please start your server to see the changes. :D";;
+wget https://jenkins.mcper.cn/job/Genisys-master/406/artifact/artifact/Genisys_1.1dev-406.phar && mv 'Genisys_1.1dev-406.phar' PocketMine-MP.phar && echo "Genisys Phar Change succeeded! Please start your server to see the changes. :D";;
 
     2) echo "Okay! Please make sure of that. If you have completed it, please run the installer again :D.";;
 esac
