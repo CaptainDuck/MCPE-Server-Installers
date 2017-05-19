85907fe837f3547c47e3c2188363ae2de6f53778
CaptainDuck <jmdimayuga021@gmail.com>
2017-05-19 14:18:36 +0800

    Update

--- Installers/PocketMine-Installer.sh
+++ Installers/PocketMine-Installer.sh
@@ -2,7 +2,7 @@
 yum install -y perl gcc g++ gcc-c++ make automake libtool autoconf m4 gcc-multilib screen bison
 mkdir server #We Make the Directory "server" for the PocketMine Directory
 cd server #To enter the Directory
-wget https://raw.githubusercontent.com/PocketMine/php-build-scripts/master/installer.sh
+wget -q -O - https://raw.githubusercontent.com/pmmp/php-build-scripts/master/installer.sh
 chmod +x installer.sh && ./installer.sh -r
 echo "Just do ./start.sh then Setup wizard will run! Good Luck! :D"
 echo "If you have accidentally crashed/stop your server, please do screen ./start.sh or screen -S then ./start.sh, To go back to your server console/screen, Do screen -r or screen -r (screen name)"
