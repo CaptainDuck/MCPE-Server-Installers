005d5e00c01cdcdcd2e8db72ea8158fad10f0d79
CaptainDuck <jmdimayuga021@gmail.com>
2017-12-09 12:22:56 +0800

    Update..

--- Installers/AllInOne/MCPEInstall.sh
+++ Installers/AllInOne/MCPEInstall.sh
@@ -2,20 +2,14 @@
 echo "Running All-In-One MCPE Server Install Script!"
 echo "Hi $USER! What MCPE Server Software would you like to install?" #Software
 echo "1) PocketMine"
-echo "2) ImagicalMine"
-echo "3) Genisys"
-echo "4) Nukkit"
-echo "5) Nevermind."
-echo "Note: Please enter it using numbers." #1-5
-echo "If you have any Problems/Issues/Questions just go to github.com/CaptainDuck/MCPE-Installers ! :D"
+echo "2) Nukkit"
+echo "3) Nevermind."
+echo "Note: Please enter it using numbers."
+echo "If you have any Problems/Issues/Questions just go to https://github.com/CaptainDuck/MCPE-Installers ! :D"
 read software;
 
 case $software in
-    1) echo "You have selected the PocketMine Software! Installing it right now!" && wget http://ws.captainduck.xyz/uploads/5/4/8/6/54868769/pocketmine-installer.sh && chmod +x pocketmine-installer.sh && ./pocketmine-installer.sh;;
-    2) echo "You have selected the ImagicalMine Software! Installing it right now!" && wget http://ws.captainduck.xyz/uploads/5/4/8/6/54868769/pocketmine-installer.sh && chmod +x pocketmine-installer.sh && ./pocketmine-installer.sh && wget http://ws.captainduck.xyz/uploads/5/4/8/6/54868769/imagicalmine-installer.sh && chmod +x imagicalmine-installer.sh && ./imagicalmine-installer.sh && 1 && echo "Just do ./start.sh then Setup wizard will run! Good Luck! :D"
-echo "If you have accidentally crashed/stop your server, please do screen ./start.sh or screen -S then ./start.sh, To go back to your server console/screen, Do screen -r or screen -r (screen name)";;
-    3) echo "You have selected the Genisys Software! Installing it right now!" && wget http://ws.captainduck.xyz/uploads/5/4/8/6/54868769/pocketmine-installer.sh && chmod +x pocketmine-installer.sh && ./pocketmine-installer.sh && wget http://ws.captainduck.xyz/uploads/5/4/8/6/54868769/genisys-installer.sh && chmod +x genisys-installer.sh && ./genisys-installer.sh && 1 && echo "Just do ./start.sh then Setup wizard will run! Good Luck! :D"
-echo "If you have accidentally crashed/stop your server, please do screen ./start.sh or screen -S then ./start.sh, To go back to your server console/screen, Do screen -r or screen -r (screen name)";;
-    4) echo "You have selected the Nukkit Software! Installing it right now!" && wget http://ws.captainduck.xyz/uploads/5/4/8/6/54868769/nukkit-installer.sh && chmod +x nukkit-installer.sh && ./nukkit-installer.sh;;
-    5) exit
+    1) echo "You have selected the PocketMine Server Software! Installing it right now!" && wget https://www.duckyfiles.website/cloud/files/pm-installer/dl/PocketMine-Installer.sh && chmod +x PocketMine-Installer.sh && ./PocketMine-Installer.sh;;
+    2) echo "You have selected the Nukkit Server Software! Installing it right now!" && wget https://www.duckyfiles.website/cloud/files/nukkit-installer/dl/Nukkit-Installer.sh && chmod +x Nukkit-Installer.sh && ./Nukkit-Installer.sh;;
+    3) exit
 esac
