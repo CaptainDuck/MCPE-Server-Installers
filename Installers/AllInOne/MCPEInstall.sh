d1cfc3f69ca96b794c99972269ea5b9acbbe73c7
CaptainDuck <jmdimayuga021@gmail.com>
2018-03-01 20:09:13 +0800

    Update, ready for use :)

--- Installers/AllInOne/MCPEInstall.sh
+++ Installers/AllInOne/MCPEInstall.sh
@@ -5,11 +5,11 @@ echo "1) PocketMine"
 echo "2) Nukkit"
 echo "3) Nevermind."
 echo "Note: Please enter it using numbers."
-echo "If you have any Problems/Issues/Questions just go to https://github.com/CaptainDuck/MCPE-Installers ! :D"
+echo "If you have any Problems/Issues/Questions just go to https://github.com/CaptainDuck/MCPE-Server-Installers ! :D"
 read software;
 
 case $software in
-    1) echo "You have selected the PocketMine Server Software! Installing it right now!" && wget https://www.duckyfiles.website/cloud/files/pm-installer/dl/PocketMine-Installer.sh && chmod +x PocketMine-Installer.sh && ./PocketMine-Installer.sh;;
-    2) echo "You have selected the Nukkit Server Software! Installing it right now!" && wget https://www.duckyfiles.website/cloud/files/nukkit-installer/dl/Nukkit-Installer.sh && chmod +x Nukkit-Installer.sh && ./Nukkit-Installer.sh;;
+    1) echo "You have selected the PocketMine Server Software! Installing it right now!" && wget https://github.com/CaptainDuck/MCPE-Server-Installers/raw/master/Installers/PocketMine-Installer.sh.sh && chmod +x PocketMine-Installer.sh && ./PocketMine-Installer.sh;;
+    2) echo "You have selected the Nukkit Server Software! Installing it right now!" && wget https://github.com/CaptainDuck/MCPE-Server-Installers/raw/master/Installers/Nukkit-Installer.sh && chmod +x Nukkit-Installer.sh && ./Nukkit-Installer.sh;;
     3) exit
 esac
